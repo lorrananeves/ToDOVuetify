6927e67a75d7d7ca9fa240e7667af4b18641dee4
lorrana neves <lorrana.neves@buser.com.br>
2022-09-12 22:30:04 -0300

    first commit

---
+++ src/components/TarefaLista.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active"></v-checkbox>
+            </v-list-item-action>
+          
+            <v-list-item-content>
+              <v-list-item-title>Notifications</v-list-item-title>
+              <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-list
+      flat
+      subheader
+    >
+
+      <v-list-item-group
+        v-model="settings"
+        multiple
+        active-class=""
+      >
+<TarefaLista />
+
+        
+      </v-list-item-group> 
+    </v-list>
+  </div>
+</template>
+
+<script>
+import TarefaLista from '.../components/tarefas/TarefaLista.vue'
+  export default {
+    name: 'HomeView',
+
+    components: {
+      TarefaLista
+    },
+  }
+</script>
